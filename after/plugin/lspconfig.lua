7501a5ef980a11c24bcb0a679595693bee3f7b56
Mattio <78447457+Mattio-cmd@users.noreply.github.com>
2023-11-29 01:13:08 +0000

    First commit

---
+++ after/plugin/lspconfig.lua
@@ -0,0 +1,168 @@
+-- lspconfig.rc
+vim.lsp.set_log_level("debug")
+require'lspconfig'.pyright.setup{}
+
+--lualsp
+require'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
+}
+
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+require'lspconfig'.html.setup{
+    capabilities = capabilities,
+      configurationSection = { "html", "css", "javascript" },
+  embeddedLanguages = {
+    css = true,
+    javascript = true
+  },
+  provideFormatter = true
+}
+
+require'lspconfig'.clangd.setup{}
+--require'lspconfig'.hls.setup{} -- Haskell lsp
+--require'lspconfig'.tsserver.setup{
+    --hostInfo = "neovim",
+--}
+
+require'lspconfig'.texlab.setup{
+    texlab = {
+    auxDirectory = ".",
+    bibtexFormatter = "texlab",
+    build = {
+      args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+      executable = "latexmk",
+      forwardSearchAfter = false,
+      onSave = false
+    },
+    chktex = {
+      onEdit = false,
+      onOpenAndSave = false
+    },
+    diagnosticsDelay = 300,
+    formatterLineLength = 80,
+    forwardSearch = {
+      args = {}
+    },
+    latexFormatter = "latexindent",
+    latexindent = {
+      modifyLineBreaks = false
+    }
+  }
+}
+
+require'lspconfig'.rust_analyzer.setup{}
+require'lspconfig'.eslint.setup{
+    capabilities = capabilities,
+}
+
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
+--require'lspconfig'.racket_langserver.setup{}
+--require'lspconfig'.dartls.setup{}
+--require'lspconfig'.flow.setup{}
+
+local nvim_lsp = require('lspconfig')
+local protocol = require'vim.lsp.protocol'
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  --Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  --buf_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  --buf_set_keymap('n', '<C-j>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', '<S-C-j>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+  -- formatting
+  if client.name == 'tsserver' then
+    client.resolved_capabilities.document_formatting = false
+  end
+
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_command [[augroup Format]]
+    vim.api.nvim_command [[autocmd! * <buffer>]]
+    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    vim.api.nvim_command [[augroup END]]
+  end
+
+ --protocol.SymbolKind = { }
+  protocol.CompletionItemKind = {
+    '', -- Text
+    '', -- Method
+    '', -- Function
+    '', -- Constructor
+    '', -- Field
+    '', -- Variable
+    '', -- Class
+    'ﰮ', -- Interface
+    '', -- Module
+    '', -- Property
+    '', -- Unit
+    '', -- Value
+    '', -- Enum
+    '', -- Keyword
+    '﬌', -- Snippet
+    '', -- Color
+    '', -- File
+    '', -- Reference
+    '', -- Folder
+    '', -- EnumMember
+    '', -- Constant
+    '', -- Struct
+    '', -- Event
+    'ﬦ', -- Operator
+    '', -- TypeParameter
+  }
+end
---
+++ lua/settings.lua
@@ -0,0 +1,141 @@
+-- Always display the status line
+vim.o.laststatus = 3
+
+-- Enables syntax highlighting
+vim.cmd('syntax enable')
+
+-- Required to keep multiple buffers open multiple buffers
+vim.o.hidden = true
+
+-- Display long lines as just one line
+vim.o.wrap = false
+
+-- The encoding displayed
+vim.o.encoding = 'utf-8'
+
+-- Makes popup menu smaller
+vim.o.pumheight = 10
+
+-- The encoding written to file
+vim.o.fileencoding = 'utf-8'
+
+-- Show the cursor position all the time
+vim.o.ruler = true
+
+-- More space for displaying messages
+vim.o.cmdheight = 1
+
+-- Treat dash separated words as a word text object
+--vim.o.iskeyword = vim.o.iskeyword .. '-'
+
+-- Enable your mouse
+vim.o.mouse = 'a'
+
+-- Horizontal splits will automatically be below
+vim.o.splitbelow = true
+
+-- Vertical splits will automatically be to the right
+vim.o.splitright = true
+
+-- Support 256 colors
+vim.o.t_Co = 256
+
+-- So that I can see ` conceal level in markdown files
+vim.o.conceallevel = 0
+
+-- Insert 2 spaces for a tab
+vim.o.tabstop = 2
+
+-- Change the number of space characters inserted for indentation
+vim.o.shiftwidth = 2
+
+-- Makes tabbing smarter will realize you have 2 vs 4
+vim.o.smarttab = true
+
+-- Converts tabs to spaces
+vim.o.expandtab = true
+
+-- Makes indenting smart
+vim.o.smartindent = true
+
+-- Good auto indent
+vim.o.autoindent = true
+
+-- Status line configuration
+vim.o.statusline = '%m %F %m%y %{&fileencoding?&fileencoding:&encoding}%T %=%(C:%c L:%l %P%)'
+
+-- Line numbers
+vim.o.number = true
+
+-- Enable highlighting of the current line
+vim.o.cursorline = true
+
+-- Tell Neovim what the background color looks like
+vim.o.background = 'dark'
+
+-- Always show tabs
+vim.o.showtabline = 3
+
+-- We don't need to see things like -- INSERT -- anymore
+vim.o.showmode = false
+
+-- This is recommended by coc
+vim.o.backup = false
+
+-- This is recommended by coc
+vim.o.writebackup = false
+
+-- Faster completion
+vim.o.updatetime = 300
+
+-- By default timeoutlen is 1000 ms
+vim.o.timeoutlen = 500
+
+-- Stop newline continuation of comments
+vim.o.formatoptions = vim.o.formatoptions:gsub("[cro]", "")
+
+-- Copy paste between Neovim and everything else
+vim.o.clipboard = 'unnamedplus'
+
+-- Your working directory will always be the same as your working directory
+vim.o.autochdir = true
+
+-- Enable reading .exrc files
+vim.o.exrc = true
+
+vim.o.cmdwinheight = 1
+vim.o.wildmenu = true
+vim.o.nolist = true
+vim.o.wildmode = 'longest:full,full'
+vim.o.secure = true
+vim.o.relativenumber = true
+vim.o.colorcolumn = '110'
+vim.o.smartcase = true
+vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.incsearch = true
+
+-- Highlight ColorColumn background
+vim.cmd('highlight ColorColumn ctermbg=gray')
+
+-- Auto source when writing to init.vim; alternatively, you can run :source $MYVIMRC
+vim.api.nvim_exec([[
+augroup auto_source
+  autocmd!
+  autocmd BufWritePost $MYVIMRC source %
+augroup END
+]], false)
+
+-- You can't stop me
+vim.api.nvim_set_keymap('c', 'w!!', 'w !doas tee %', { noremap = true, silent = true })
+
+-- Header files would be recognized as cpp
+vim.api.nvim_exec([[
+augroup project
+  autocmd!
+  autocmd BufRead,BufNewFile *.h,*.c set filetype=c
+augroup END
+]], false)
+
+-- Set markdown fenced languages
+vim.g.markdown_fenced_languages = {'typescript', 'c', 'python', 'cpp'}
